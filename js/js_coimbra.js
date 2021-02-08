e5a3f467098d6a3cbfb77026866c19bded48054c
dngea <66570410+dngea@users.noreply.github.com>
2021-02-08 15:26:08 +0000

    navegation interactive, buttons added

--- js/js_coimbra.js
+++ js/js_coimbra.js
@@ -7,5 +7,17 @@ selector2.addEventListener("mouseout", () => {
   selector1.className = "selector-1";
 }, false);
 
+var selector4 = document.getElementById("selector4");
+var selector3 = document.getElementById("selector3");
+selector4.addEventListener("mouseover", () => {
+  selector3.className = "selector3change";
+}, false)
+selector4.addEventListener("mouseout", () => {
+  selector3.className = "selector-3";
+}, false);
+
 document.getElementById("selector1").setAttribute('onclick', 'location.href = "coimbracor.html"'); 
 document.getElementById("selector2").setAttribute('onclick', 'location.href = "coimbragaleria.html"'); 
+
+document.getElementById("selector3").setAttribute('onclick', 'location.href = "coimbracor.html"'); 
+document.getElementById("selector4").setAttribute('onclick', 'location.href = "coimbragaleria.html"');
