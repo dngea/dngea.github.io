b90b90e3112cd304ca20d861faa076ea10fdca5e
dngea <66570410+dngea@users.noreply.github.com>
2021-02-09 21:30:12 +0000

    galeria

--- js/js_coimbra.js
+++ js/js_coimbra.js
@@ -1,3 +1,8 @@
+document.getElementById("selector1").setAttribute('onclick', 'location.href = "coimbracor.html"'); 
+document.getElementById("selector2").setAttribute('onclick', 'location.href = "coimbragaleria.html"'); 
+
+
+
 var selector2 = document.getElementById("selector2");
 var selector1 = document.getElementById("selector1");
 selector2.addEventListener("mouseover", () => {
@@ -6,6 +11,9 @@ selector2.addEventListener("mouseover", () => {
 selector2.addEventListener("mouseout", () => {
   selector1.className = "selector-1";
 }, false);
+
+document.getElementById("selector3").setAttribute('onclick', 'location.href = "coimbracor.html"'); 
+document.getElementById("selector4").setAttribute('onclick', 'location.href = "coimbragaleria.html"');
 
 var selector4 = document.getElementById("selector4");
 var selector3 = document.getElementById("selector3");
@@ -16,8 +24,5 @@ selector4.addEventListener("mouseout", () => {
   selector3.className = "selector-3";
 }, false);
 
-document.getElementById("selector1").setAttribute('onclick', 'location.href = "coimbracor.html"'); 
-document.getElementById("selector2").setAttribute('onclick', 'location.href = "coimbragaleria.html"'); 
 
-document.getElementById("selector3").setAttribute('onclick', 'location.href = "coimbracor.html"'); 
-document.getElementById("selector4").setAttribute('onclick', 'location.href = "coimbragaleria.html"');
+
