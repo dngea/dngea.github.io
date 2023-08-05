d04ef6a424d1f6985849eae4abdacb1584009c84
dngea <66570410+dngea@users.noreply.github.com>
2023-08-05 17:19:26 +0200

    Portfolio (stage 02)

---
+++ js/main.js
@@ -0,0 +1,30 @@
+function openNav() {
+    document.getElementById("overlay_nav").style.height = "100vh";
+    document.getElementById("overlay_nav").style.visibility = "visible";
+  }
+  
+  function closeNav() {
+    document.getElementById("overlay_nav").style.height = "0vh";
+    document.getElementById("overlay_nav").style.visibility = "hidden";
+  }
+
+//   function border(){
+//     document.getElementById("overlayBorder").style.borderBottom = "15px";
+//   }
+
+
+  gsap.utils.toArray(".slideText").forEach(text => {
+    gsap.timeline({
+      defaults: {ease: "none"},
+      scrollTrigger: {
+        scroller: text.closest(".horizSlider"),
+        horizontal: true,
+        trigger: text.closest(".slide"),
+        start: "left right",
+        end: "left left",
+        scrub: true
+      }
+    })
+    .fromTo(text, {x: 250}, {x: -250}, 0)
+    .from(text.nextElementSibling, {scale: 0.8}, 0)
+  });
