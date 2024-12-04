71774383020d7c432e326dbac86529ddb2482110
dngea <66570410+dngea@users.noreply.github.com>
2024-12-04 02:39:05 +0100

    again

--- js/main.js
+++ js/main.js
@@ -18,24 +18,6 @@ function openNav() {
   });
 
 
-  // gsap.utils.toArray(".slideText").forEach(text => {
-  //   gsap.timeline({
-  //     defaults: {ease: "none"},
-  //     scrollTrigger: {
-  //       scroller: text.closest(".horizSlider"),
-  //       horizontal: true,
-  //       trigger: text.closest(".slide"),
-  //       start: "left right",
-  //       end: "left left",
-  //       scrub: true
-  //     }
-  //   })
-  //   .fromTo(text, {x: 250}, {x: -250}, 0)
-  //   .from(text.nextElementSibling, {scale: 0.8}, 0)
-  // });
-
-
-
 // Función para cargar el header
 function loadHeader() {
   console.log("loadHeader function called"); // Verificar si se llama la función
